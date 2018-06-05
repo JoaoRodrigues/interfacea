ac43d718452a5f472703164725e90aaf0c9a51d3
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2018-06-04 20:42:34 -0700

    Added module parent logger for debugging purposes

--- interfacea/__init__.py
+++ interfacea/__init__.py
@@ -5,12 +5,23 @@ Code to calculate pairwise residue energies in macromolecular
 structures.
 """
 
+import logging
 import os
 
 import simtk.openmm.app as app
 
 from .structure import Structure
 from .structure import StructureError
+
+
+# Setup logger
+# This is the parent logger since the library is supposed
+# to be loaded from here. Hence, configs set here apply to
+# all module-level loggers
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+# Methods
 
 
 def read(fpath, ftype=None):
@@ -33,8 +44,9 @@ def read(fpath, ftype=None):
     _pdb_formats = {'pdb', 'ent'}
     _cif_formats = {'cif', 'mmcif'}
     _formats = _pdb_formats | _cif_formats
+    _formats_str = ','.join(_formats)
 
-    exts = ','.join(_formats)
+    logger.debug('Reading file: {}'.format(fpath))
 
     fullpath = os.path.abspath(fpath)
     if not os.path.isfile(fullpath):
@@ -43,10 +55,12 @@ def read(fpath, ftype=None):
 
     fname, fext = os.path.splitext(fullpath)
     ftype = ftype if ftype is not None else fext[1:]
+    logger.debug('Assigning file type: {}'.format(ftype))
 
     if ftype in _pdb_formats:
         try:
             struct = app.PDBFile(fullpath)
+            logger.debug('Successfully parsed file as PDB')
         except Exception as e:
             emsg = 'Failed parsing file {} as \'PDB\' format'.format(fullpath)
             raise StructureError(emsg, e)
@@ -54,11 +68,12 @@ def read(fpath, ftype=None):
     elif ftype in _cif_formats:
         try:
             struct = app.PDBxFile(fullpath)
+            logger.debug('Successfully parsed file as mmCIF')
         except Exception as e:
             emsg = 'Failed parsing file {} as \'mmCIF\' format'.format(fullpath)
             raise StructureError(emsg, e)
     else:
-        emsg = '\'{}\' is not one of the supported types: {}'.format(ftype, exts)
+        emsg = '\'{}\' is not one of the supported types: {}'.format(ftype, _formats_str)
         raise StructureError(emsg)
 
     return Structure(fullpath, struct)
