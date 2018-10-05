6ec62377de82335380bd32fd80c94b1918541f03
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2018-10-05 16:31:49 +0200

    Refactored code to use kdtree implementation

--- interfacea/interactions.py
+++ interfacea/interactions.py
@@ -70,112 +70,11 @@ class InteractionAnalyzer(object):
                 emsg = '\'table\' argument should be an InteractionTable() instance'
                 raise InteractionAnalyzerError(emsg)
 
-        self.atom_neighbors = None
-        self.neighbors = None
-
-        self.interactions = None  # dictionary of contacts/type
-
-        self.atomic_charges = None
-        self.charged_residues = None
-
-        # Convert positions to numpy array
-        _xyz_list = structure.positions.value_in_unit(units.angstrom)
-        self._positions = np.asarray(_xyz_list, dtype=np.float32)
-        del _xyz_list
-
         logging.debug('New InteractionAnalyzer for structure: \'{}\''.format(structure.name))
-
-    #
-    # Neighbor Search Functions
-    #
-    def get_neighboring_atoms(self, cutoff=5.0):
-        """Calculates neighboring atoms based on a minimum distance threshold.
-
-        Args:
-            cutoff (float): minimum distance in Angstrom to consider
-                two atoms in contact.
-        """
-
-        n, m = self._positions.shape
-        gram_matrix = np.dot(self._positions, self._positions.T)
-        h_matrix = np.tile(np.diag(gram_matrix), (n, 1))
-        d_mtx = h_matrix + h_matrix.T - 2 * gram_matrix
-
-        cutoff_sq = cutoff * cutoff
-        atom_neighbors = np.argwhere(d_mtx <= cutoff_sq).tolist()
-
-        # Clean list of (i,i) and (i,j)/(j,i) duplicates
-        _unique = set()
-        for pair in atom_neighbors:
-            at_i, at_j = pair
-            pair, pair_r = (at_i, at_j), (at_j, at_i)
-
-            if (pair == pair_r) or (pair in _unique) or (pair_r in _unique):
-                continue
-            _unique.add(pair)
-
-        del atom_neighbors, d_mtx, gram_matrix, h_matrix
-        self.atom_neighbors = sorted(_unique)
-
-        logging.debug('Found {} pairs of neighboring atoms at cutoff of {:3.1f}A'.format(len(_unique), cutoff))
-
-    def get_neighboring_residues(self, cutoff=5.0, skip_intra=True):
-        """Calculates neighboring residues using minimum atom distances.
-
-        Args:
-            cutoff (float): minimum distance in Angstrom to consider
-                two atoms in contact and the two residues in contact.
-
-            skip_intra (bool): flag to exclude(default)/include intra-chain
-                contacts.
-
-        """
-
-        def _res_sort_key(residue_pair):
-            """Private method to sort pairs of residues
-            """
-            res_i, res_j = residue_pair
-
-            i_key = (res_i.chain.id, res_i.id)
-            j_key = (res_j.chain.id, res_j.id)
-            return (i_key, j_key)
-
-        if self.atom_neighbors is None:
-            self.get_neighboring_atoms(cutoff=cutoff)
-
-        neighbors = {res: [] for res in self.structure.topology.residues()}
-        atoms = list(self.structure.topology.atoms())
-
-        for pair in self.atom_neighbors:
-            at_i, at_j = pair
-            atom_i, atom_j = atoms[at_i], atoms[at_j]
-
-            if skip_intra and (atom_i.residue.chain.id == atom_j.residue.chain.id):
-                continue
-
-            if atom_i.residue == atom_j.residue:
-                continue
-
-            if atom_j.residue in neighbors[atom_i.residue]:
-                continue
-
-            neighbors[atom_i.residue].append(atom_j.residue)
-            neighbors[atom_j.residue].append(atom_i.residue)
-
-        self.neighbors = neighbors
-
-        logging.debug('Propagated atom neighbors to residue level')
 
     #
     # Interaction Calculators
     #
-
-    def _calc_sq_atomic_distance(self, atom_a, atom_b):
-        """Returns the squared euclidean distance between two atoms
-        """
-
-        pos_diff = self._positions[atom_a.index] - self._positions[atom_b.index]
-        return np.sum(pos_diff * pos_diff)
 
     def _detect_rings(self, residue):
         """Construct spanning tree to determine rings.
@@ -225,8 +124,8 @@ class InteractionAnalyzer(object):
 
         return atoms_in_rings
 
-    def find_salt_bridges(self, anions=None, cations=None, cutoff=4.0):
-        """Method to find salt bridges in the structure.
+    def get_ionic(self, anions=None, cations=None, cutoff=4.0, intramolecular=False, intermolecular=True):
+        """Method to detect ionic interactions/salt bridges in the structure.
 
         Finds potentially charged groups using a list of pre-defined or
         user-supplied functional groups. Functional groups should be defined
@@ -239,6 +138,16 @@ class InteractionAnalyzer(object):
 
         The structure is then searched for pairs of opposing charge groups with
         N/O atoms within, by default, 4 Angstrom of each other (Barlow & Thorton, 1983).
+
+        Args:
+            anions (list(:obj: `FunctionalGroup`)): list of anionic groups to match in structure.
+            cations (list(:obj: `FunctionalGroup`)): list of cationic groups to match in structure.
+            cutoff (float): maximum distance for N/O pairs in matched groups, in Angstrom.
+                Default is 4.0 Angstrom
+            intramolecular (bool): find ionic interactions *within* the same chain.
+                Default is False.
+            intermolecular (bool): find ionic interactions only between different chains.
+                Default is True.
         """
 
         # Instantiate ionizable functional groups
@@ -258,10 +167,6 @@ class InteractionAnalyzer(object):
             _i = fgs.Imidazolium()
 
             cations = (_a, _g, _i)
-
-        # Run methods if necessary
-        if self.neighbors is None:
-            self.get_neighboring_residues()
 
         # Iterate over residues and find ionizable groups
         res_pos, res_neg = {}, {}
@@ -286,30 +191,40 @@ class InteractionAnalyzer(object):
 
         # Find pairs that are close in space.
         # Use threshold of 4A between N/O atoms
-        _get_sq_dij = self._calc_sq_atomic_distance
-        _sq_cutoff = cutoff * cutoff
         _n_or_o = (7, 8)
-        _nsb = 0  # number of salt-bridges
+        _num = 0  # number of interactions for logging
         for res_i, cation in res_pos.items():
-            neighbors = self.neighbors[res_i]
-            i_n_and_o = [a for a in cation if a.element.atomic_number in _n_or_o]
+            chain_i = res_i.chain.id
+            n_and_o = [a for a in cation if a.element.atomic_number in _n_or_o]
+            _c, _n, _i, _num_no = chain_i, res_i.name, res_i.id, len(n_and_o)
+            logging.debug('Searching neighbors of {}:{}{} ({} N/O)'.format(_c, _n, _i, _num_no))
 
-            for res_j, anion in res_neg.items():
-                if res_j not in neighbors:
+            neighbors = self.structure.get_neighbors(n_and_o, radius=cutoff, level='atom')
+
+            _seen = set()
+            for n in neighbors:
+                res_j = n.residue
+                chain_j = res_j.chain.id
+                if ((res_j not in res_neg) or (res_j in _seen) or
+                   (not intramolecular and chain_i == chain_j) or
+                   (not intermolecular and chain_i != chain_j)):
                     continue
 
-                j_n_and_o = [a for a in anion if a.element.atomic_number in _n_or_o]
-                pairs = itertools.product(i_n_and_o, j_n_and_o)
-                for ai, aj in pairs:
-                    sq_d = _get_sq_dij(ai, aj)
-                    logging.debug('({})[+] = ({})[-] == {}'.format(res_i, res_j, sq_d))
-                    if sq_d <= _sq_cutoff:
-                        self.itable.add(res_i, res_j, 'salt-bridge')
-                        _nsb += 1
-                        break
+                j_n_and_o = {a for a in res_neg[res_j] if a.element.atomic_number in _n_or_o}
 
-        # self.salt_bridges = salt_bridges
-        logging.debug('Found {} salt-bridge(s) in structure'.format(_nsb))
+                if n in j_n_and_o:
+                    logging.debug('({})[+] = ({})[-]'.format(res_i, res_j))
+                    self.itable.add(res_i, res_j, 'ionic')
+                    _num += 1
+                    _seen.add(res_j)
+
+        logging.info('Found {} ionic interaction(s) in structure'.format(_num))
+
+
+class ResidueTable(object):
+    """Table-like container object to hold interaction information per-residue.
+    """
+    pass
 
 
 class InteractionTable(object):
@@ -322,22 +237,26 @@ class InteractionTable(object):
     def __init__(self, name=None):
         self.name = name
 
-        # Setup DataFrame
+        self._setup_new_df()
+
+    def _setup_new_df(self):
+        """Creates a new DataFrame.
+        """
+
         _col = ['itype',
                 'chain_a', 'chain_b', 'resname_a', 'resname_b',
-                'resid_a', 'resid_b', 'energy']
+                'resid_a', 'resid_b']
 
         self._table = pd.DataFrame(columns=_col)
 
-    def add(self, res_a, res_b, itype=None, ienergy=None):
+    def add(self, res_a, res_b, itype=None):
         """Appends an interaction type to the table.
 
         Args:
             res_a (:obj:`Residue`): `Residue` object involved in the interaction.
             res_b (:obj:`Residue`): Other `Residue` object involved in the interaction.
 
-            itype (str): name of the interaction type (e.g. salt-bridge)
-            ienergy (float): energy value of the interaction.
+            itype (str): name of the interaction type (e.g. ionic)
         """
 
         # Sort residues by chain/number
@@ -350,9 +269,16 @@ class InteractionTable(object):
         df = self._table
         df.loc[len(df)] = [itype,
                            chain_a, chain_b, resname_a, resname_b,
-                           resid_a, resid_b, ienergy]
+                           resid_a, resid_b]
 
         logging.debug('InteractionTable now contains {} entries'.format(len(df)))
+
+    def clear(self):
+        """Deletes all entries in InteractionTable.
+        """
+
+        self._table = None
+        self._setup_new_df()
 
     def sort(self):
         pass
@@ -365,3 +291,24 @@ class InteractionTable(object):
 
     def to_json(self):
         pass
+
+    def compare(self, other):
+        """Performs a per-row/per-column comparison between two tables.
+
+        Proposed algorithm:
+            for this.row, other.row in (this, other)
+              if this.row not in other
+                new.row[itype] = None
+              elif this.row in other
+                compare this.row[itype] with other.row[itype] => None if diff.
+            return new
+        """
+
+        pass
+
+    def difference(self, other):
+        """Show rows only in this table
+        """
+        pass
+
+
