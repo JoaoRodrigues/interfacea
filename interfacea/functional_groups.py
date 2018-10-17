b3b8db2224bd055b526d0a95c282bda6dd5b9321
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2018-10-16 21:53:36 -0700

    Probably a final version of the FunctionalGroup object. Includes unit tests

--- interfacea/functional_groups.py
+++ interfacea/functional_groups.py
@@ -19,45 +19,131 @@ import networkx.algorithms.isomorphism as iso
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
+class FunctionalGroupError(Exception):
+    """Custom Error Class"""
+    pass
+
+
 class FunctionalGroup(object):
     """Base class to represent chemical functional groups.
 
-    Subclasses should implement a `match` method and have their
-    own list of `elements` and `bonds`.
-
-    e.g. defining an alcohol group
-
-    oh_elements = [8, 1]
-    oh_bonds = [(0, 1)]
-    alcohol = FunctionalGroup(name='-oh', elements=oh_elements, bonds=oh_bonds)
+    Instances and subclasses can implement a `match` method
+    and must define their own list of `elements` and `bonds`
+    and `max_bonds`.
 
     Attributes:
         name (str): name of the functional group.
         charge (int): formal charge of the group.
-        elements (`list(int)`): atomic numbers of the elements contained in the group.
-            A 0 (zero) acts as a wildcard, meaning any element.
+        elements (`list(int)` or `list(tuple)`): elements contained in the group.
+            Elements should be represented by their atomic number (1 - Hydrogen, 6 - Carbon, etc)
+            A '0' (zero) is the wildcard, meaning any element matches.
+            You can also pass a tuple of allowed elements, e.g. (1, 6) means match carbon or hydrogen.
         bonds (`list(tuple)`): bonds between elements of the functional group.
             Bond indexes refer to the positions of the elements in the `elements` set.
-        terminal (`set(int)`): set of indexes of atoms with only one connection (terminal).
-            Useful to distinguish between similar groups (e.g. esther vs carboxylate)
+        max_bonds (`list(int)`): maximum number of bonds allowed for each element.
+            Useful to restrict some searches (e.g. carboxylate vs esther). See examples below.
+            Defaults to an arbitrary large number of bonds per atom (no filtering).
+
+    Examples:
+
+        >>> # Backbone (NH-CH-C-O)
+        >>> #
+        >>> #       H    H    O
+        >>> #       |    |    |
+        >>> #    -- N -- C -- C --
+        >>> #            |
+        >>> #            X
+        >>> #
+        >>> elements = [7, 1, 6, 1, 0, 6, 8]
+        >>> bonds = [(0, 1), (0, 2), (2, 3), (2, 4), (2, 5), (5, 6)]
+
+        >>> bb = fgs.FunctionalGroup(name='backbone',
+                                     charge=0,
+                                     elements=elements,
+                                     bonds=bonds)
+
+
+        >>> # Esther (X-O-O-X)
+        >>> #
+        >>> #         O
+        >>> #        /
+        >>> #    -- C -- O -- X
+        >>> #
+        >>> elements = [6, 8, 8, 0]
+        >>> bonds = [(0, 1), (0, 2), (2, 3)]
+
+        >>> esther = fgs.FunctionalGroup(name='esther',
+                                         charge=0,
+                                         elements=elements,
+                                         bonds=bonds)
+
+        >>> # Carboxylate (X-O-O)
+        >>> #
+        >>> #         O
+        >>> #        /
+        >>> #    -- C -- O
+        >>> #
+        >>> elements = [6, 8, 8]
+        >>> bonds = [(0, 1), (0, 2)]
+        >>> max_bonds = [3, 1, 1]
+
+        >>> coo = fgs.FunctionalGroup(name='carboxylate',
+                                      charge=0,
+                                      elements=elements,
+                                      bonds=bonds
+                                      max_bonds=max_bonds)
+
     """
 
-    __slots__ = ['name', 'charge', 'elements', 'bonds', 'terminal']
+    __slots__ = ['name', 'charge', 'elements',
+                 'bonds', 'max_bonds',
+                 '_element_set', '_g']
 
-    def __init__(self, name=None, charge=None, elements=None, bonds=None, terminal=None):
+    def __init__(self, name=None, charge=None, elements=None, bonds=None, max_bonds=None):
 
-        assert name is not None, "FunctionalGroup must have a name!"
-        assert charge is not None, "FunctionalGroup must have a charge!"
-        assert elements is not None, "FunctionalGroup must have atoms!"
-        assert bonds is not None, "FunctionalGroup must have bonds!"
+        assert name is not None, 'FunctionalGroup must have a name!'
+        assert charge is not None, 'FunctionalGroup must have a charge!'
+        assert elements is not None, 'FunctionalGroup must have atoms!'
+        assert bonds is not None, 'FunctionalGroup must have bonds!'
 
         self.name = name
         self.elements = elements
         self.bonds = bonds
         self.charge = charge
-        self.terminal = set([]) if terminal is None else set(terminal)
 
-        self._element_set = set(elements)
+        if max_bonds is None:
+            max_bonds = [99 for e in elements]
+        else:
+            assert len(max_bonds) == len(elements), 'Items in max_bonds must match elements'
+        self.max_bonds = max_bonds
+
+        # Ensure all elements are bonded
+        for idx, elem in enumerate(elements):
+            in_bond = sum([1 for b in bonds if idx in b])
+            if not in_bond:
+                emsg = 'Atom #{} ({}) is not in any bond.'.format(idx, elem)
+                raise FunctionalGroupError(emsg)
+
+        # Ensure all bonds belong to existing elements
+        for idx, bond in enumerate(bonds):
+            a1, a2 = bond
+            if a1 > len(elements) or a2 > len(elements):
+                emsg = 'Bond #{} \'({}, {})\' includes an unknown atom.'
+                raise FunctionalGroupError(emsg.format(idx, a1, a2))
+
+        # Make element list a list of sets for efficient search
+        _elements = []
+        for elem in elements:
+            if isinstance(elem, int):
+                _elements.append({elem})
+            elif isinstance(elem, (list, tuple)):
+                _elements.append(set(elem))
+
+        self.elements = _elements
+
+        # Make element set to discard easily residues not containing elements
+        self._element_set = {subitem for item in self.elements for subitem in item}
+
         self._build_graph_representation()
         logging.debug('Successfully setup functional group \'{}\''.format(name))
 
@@ -82,42 +168,73 @@ class FunctionalGroup(object):
     def match(self, residue):
         """Compares and returns matches between the FG and Residue graphs.
 
-        Returns the atoms of the residue that match atoms of the functional
-        group or an empty list otherwise.
+        Does *not* match bonds across residues.
+
+        Arguments:
+            residue (:obj: `Residue`): OpenMM Residue object to scan for FG matches.
+
+        Returns:
+            All groups of atoms matching the FG in the `Residue`, as a list of lists of
+            `Atom` objects.
         """
+
+        matched_groups = []  # we can have more than one match!
 
         # Match atoms/elements first
         atomlist = list(residue.atoms())
         elemlist = {a.element.atomic_number for a in atomlist}
         if not ((self._element_set - {0}) <= elemlist):
-            return []  # not all atoms are in the Residue
+            return matched_groups  # not all atoms are in the Residue
 
         # Match fg subgraph to residue graph
         def _node_match(n1, n2):
-            """Returns True if two nodes in the graph are equivalent.
+            """Returns True if the residue atomic elements match the FG node elements.
             """
-            # Edge matches, not node match.
-            return (n1['element'] == n2['element'] or n2['element'] == 0)
+            if n1['element'] in n2['element']:
+                return True
+            elif 0 in n2['element']:
+                return True
+            else:
+                return False
 
         res_graph = residue._g
         matcher = iso.GraphMatcher(res_graph, self._g, node_match=_node_match)
+        _mb = self.max_bonds  # make local variable
         if matcher.subgraph_is_isomorphic():
 
-            matched_atoms = []
-            for r_idx, g_idx in matcher.mapping.items():
-                atom = atomlist[r_idx]
-                if g_idx in self.terminal and len(residue.bonds_per_atom.get(atom)) != 1:
-                    return []
-                matched_atoms.append(atom)
+            observed = set()  # avoid degenerate matches
+            for subgraph in matcher.subgraph_isomorphisms_iter():
+                sorted_keys = tuple(sorted(subgraph.keys()))
+                if sorted_keys in observed:
+                    continue
 
-            return matched_atoms  # return the atoms matching the functional group
+                observed.add(sorted_keys)
 
-        return []
+                matched_atoms = []
+                for r_idx, g_idx in subgraph.items():
+                    atom = atomlist[r_idx]
+
+                    # Now check if atoms make only the bonds specified in the FG
+                    num_bonds_in_res = len(residue.bonds_per_atom.get(atom))
+                    if num_bonds_in_res > _mb[g_idx]:
+                        logging.debug('Atom \'{}\' exceeds max_bonds ({}/{})'.format(atom.name,
+                                                                                     num_bonds_in_res,
+                                                                                     _mb[g_idx]))
+                        break
+
+                    matched_atoms.append(atom)
+                else:
+                    matched_groups.append(matched_atoms)
+
+            logging.debug('{} matches functional group \'{}\' {} times'.format(residue,
+                                                                               self.name,
+                                                                               len(matched_groups)))
+        return matched_groups
 
     def search(self, structure):
         """Utility function to search an entire structure matches of the functional group.
 
-        Returns a dictionary with `Residue` objects as keys and the matched atoms as values.
+        Returns a dictionary with `Residue` objects as keys and the matched groups as values.
         """
 
         matches = {}
@@ -130,8 +247,15 @@ class FunctionalGroup(object):
 
 
 # Common functional groups
+
+# Charged
 class Carboxylate(FunctionalGroup):
-    """Carboxylate Functional Group.
+    """Carboxylate.
+
+         O
+        /
+    -- C -- O
+
     """
 
     def __init__(self):
@@ -140,12 +264,16 @@ class Carboxylate(FunctionalGroup):
                          charge=-1,
                          elements=[6, 8, 8],
                          bonds=[(0, 1), (0, 2)],
-                         terminal={1, 2}
-                         )
+                         max_bonds=[3, 1, 1])
 
 
 class Carboxyl(FunctionalGroup):
-    """Carboxyl Functional Group.
+    """Carboxyl.
+
+         O -- H
+        /
+    -- C -- O
+
     """
 
     def __init__(self):
@@ -153,26 +281,36 @@ class Carboxyl(FunctionalGroup):
                          charge=0,
                          elements=[6, 8, 8, 1],
                          # the graph matching algorithm will
-                         # match both 8(1)-1 or 8(2)-1
+                         # match either oxygens to the hydrogen
                          # effectively having 'fuzzy' edges
-                         bonds=[(0, 1), (0, 2), (1, 3)]
-                         )
+                         bonds=[(0, 1), (0, 2), (1, 3)],
+                         max_bonds=[3, 1, 2, 1])
 
 
 class Guanidinium(FunctionalGroup):
-    """Guanidinium Functional Group.
+    """Guanidinium.
+
+          H   H
+           \ /
+       H    N
+       |    |
+    -- N -- C
+            |
+            N
+           / \
+          H   H
     """
 
     def __init__(self):
         super().__init__(name='guanidinium',
                          charge=1,
-                         elements=[6, 7, 7, 7, 1, 1, 1, 1],
-                         bonds=[(0, 1), (0, 2), (0, 3), (1, 4), (1, 5), (2, 6), (3, 7)]
-                         )
+                         elements=[7, 1, 6, 7, 7, 1, 1, 1, 1],
+                         bonds=[(0, 1), (0, 2), (2, 3), (2, 4), (3, 5), (3, 6), (4, 7), (4, 8)],
+                         max_bonds=[3, 1, 3, 3, 3, 1, 1, 1, 1])
 
 
 class Imidazole(FunctionalGroup):
-    """Imidazole Functional Group.
+    """Imidazole.
 
     Without protons to avoid ambiguity between ND/NE protonation.
     User can check for position of proton later.
@@ -182,85 +320,122 @@ class Imidazole(FunctionalGroup):
         super().__init__(name='imidazole',
                          charge=0,
                          elements=[6, 6, 7, 6, 7],
-                         bonds=[(0, 1),
-                                (1, 2),
-                                (2, 3),
-                                (3, 4),
-                                (4, 0)]
-                         )
+                         bonds=[(0, 1), (1, 2), (2, 3), (3, 4), (4, 0)],
+                         max_bonds=[3, 3, 3, 3, 3])
 
 
 class Imidazolium(FunctionalGroup):
-    """Imidazolium Functional Group.
+    """Imidazolium.
     """
 
     def __init__(self):
-        super().__init__(name='imidazole',
+        super().__init__(name='imidazolium',
                          charge=1,
                          elements=[6, 6, 7, 6, 7, 1, 1, 1, 1],
                          bonds=[(0, 1),
                                 (1, 2), (1, 5),
                                 (2, 3), (2, 6),
                                 (3, 4), (3, 7),
-                                (4, 0), (4, 8)]
-                         )
+                                (4, 0), (4, 8)],
+                         max_bonds=[3, 3, 3, 3, 3, 1, 1, 1, 1])
 
 
 class Phosphate(FunctionalGroup):
-    """Phosphate Functional Group.
+    """Phosphate.
     """
 
     def __init__(self):
         super().__init__(name='phosphate',
                          charge=2,
                          elements=[15, 8, 8, 8],
-                         bonds=[(0, 1), (0, 2), (0, 3)]
-                         )
+                         bonds=[(0, 1), (0, 2), (0, 3)],
+                         max_bonds=[4, 1, 1, 1])
 
 
 class HydrogenPhosphate(FunctionalGroup):
-    """Hydrogen phosphate Functional Group.
+    """Hydrogen phosphate.
     """
 
     def __init__(self):
         super().__init__(name='phosphate-h',
                          charge=1,
                          elements=[15, 8, 8, 8, 1],
-                         bonds=[(0, 1), (0, 2), (0, 3), (1, 4)]
-                         )
+                         bonds=[(0, 1), (0, 2), (0, 3), (1, 4)],
+                         max_bonds=[4, 2, 1, 1, 1])
 
 
 class QuaternaryAmine(FunctionalGroup):
-    """Quaternary Amine Functional Group.
+    """Quaternary Amine.
     """
 
     def __init__(self):
         super().__init__(name='quaternary_amine',
                          charge=1,
-                         elements=[7, 1, 0, 0, 0],
-                         bonds=[(0, 1), (0, 2), (0, 3), (0, 4)]
-                         )
+                         elements=[7, 0, 0, 0, 0],
+                         bonds=[(0, 1), (0, 2), (0, 3), (0, 4)])
 
 
 class Sulfate(FunctionalGroup):
-    """Sulfate Functional Group.
+    """Sulfate.
     """
 
     def __init__(self):
         super().__init__(name='sulfate',
                          charge=1,
                          elements=[16, 8, 8, 8],
-                         bonds=[(0, 1), (0, 2), (0, 3)]
-                         )
+                         bonds=[(0, 1), (0, 2), (0, 3)],
+                         max_bonds=[4, 1, 1, 1])
 
 
 class HydrogenSulfate(FunctionalGroup):
-    """Hydrogen sulfate Functional Group.
+    """Hydrogen Sulfate.
     """
 
     def __init__(self):
         super().__init__(name='sulfate-h',
                          charge=0,
                          elements=[16, 8, 8, 8, 1],
-                         bonds=[(0, 1), (0, 2), (0, 3), (1, 4)]
-                         )
+                         bonds=[(0, 1), (0, 2), (0, 3), (1, 4)],
+                         max_bonds=[4, 2, 1, 1, 1])
+
+
+# Hydrophobic
+class DivalentSulphur(FunctionalGroup):
+    """Divalent Sulphur.
+    """
+
+    def __init__(self):
+        super().__init__(name='divalent-sulphur',
+                         charge=0,
+                         elements=[16, 0, 0],
+                         bonds=[(0, 1), (0, 2)],
+                         max_bonds=[2, 4, 4])
+
+
+class AliphaticCarbon(FunctionalGroup):
+    """Aliphatic Chain.
+    """
+
+    def __init__(self):
+        super().__init__(name='aliphatic-carbon',
+                         charge=0,
+                         elements=[6, (1, 6), (1, 6), (1, 6), (1, 6)],
+                         bonds=[(0, 1), (0, 2), (0, 3), (0, 4)])
+
+
+class Phenyl(FunctionalGroup):
+    """Phenyl Group.
+    """
+
+    def __init__(self):
+        super().__init__(name='phenyl',
+                         charge=0,
+                         elements=[6, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 0],
+                         bonds=[(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 0),
+                                (0, 6), (1, 7), (2, 8), (3, 9), (4, 10), (5, 11)])
+
+
+# Lists for easier access
+anionic = [Carboxylate, Phosphate, HydrogenPhosphate, Sulfate]
+cationic = [Guanidinium, Imidazolium, QuaternaryAmine]
+charged = anionic + cationic
---
+++ tests/test_functionalgroups.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+"""
+Unit tests for FunctionalGroup classes and module.
+"""
+
+import copy
+import os
+import tempfile
+import unittest
+import warnings
+
+import interfacea as ia
+import interfacea.functional_groups as fgs
+
+TESTDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class TestBasicFunctionalGroup(unittest.TestCase):
+    """Simple test of an instance of the FunctionalGroup class.
+    """
+
+    def setUp(self):
+        fpath = os.path.join(TESTDIR, 'data', 'mini.pdb')
+        self.struct = ia.read(fpath)
+
+    def test_assertions_and_exceptions(self):
+        """Tests badly constructed FGs.
+        """
+
+        with self.assertRaisesRegexp(AssertionError, 'FunctionalGroup must have a name!'):
+            _ = fgs.FunctionalGroup(charge=0,
+                                    elements=[1, 1],
+                                    bonds=[(0, 1)])
+
+        with self.assertRaisesRegexp(AssertionError, 'FunctionalGroup must have a charge!'):
+            _ = fgs.FunctionalGroup(name='test',
+                                    elements=[1, 1],
+                                    bonds=[(0, 1)])
+
+        with self.assertRaisesRegexp(AssertionError, 'FunctionalGroup must have atoms!'):
+            _ = fgs.FunctionalGroup(name='test',
+                                    charge=0,
+                                    bonds=[(0, 1)])
+
+        with self.assertRaisesRegexp(AssertionError, 'FunctionalGroup must have bonds!'):
+            _ = fgs.FunctionalGroup(name='test',
+                                    charge=0,
+                                    elements=[(1, 1)])
+
+        with self.assertRaisesRegexp(AssertionError, 'Items in max_bonds must match elements'):
+            _ = fgs.FunctionalGroup(name='test',
+                                    charge=0,
+                                    elements=[1, 1],
+                                    bonds=[(0, 1)],
+                                    max_bonds=[0])
+
+        with self.assertRaises(fgs.FunctionalGroupError):
+            _ = fgs.FunctionalGroup(name='isolated-element',
+                                    charge=0,
+                                    elements=[1, 1, 1],
+                                    bonds=[(0, 1)])
+
+        with self.assertRaises(fgs.FunctionalGroupError):
+            _ = fgs.FunctionalGroup(name='extra-bond',
+                                    charge=0,
+                                    elements=[(1, 1)],
+                                    bonds=[(0, 1), (0, 2)])
+
+    def test_custom_FG(self):
+        """Tests matching custom Functional Groups
+        """
+
+        # Neutral N Backbone (NH-CH-C-O)
+        #
+        #       H    H    O
+        #       |    |    |
+        #  X -- N -- C -- C --
+        #            |
+        #            X
+        #
+        elements = [7, 1, 6, 1, 6, 0, 8]
+        bonds = [(0, 1), (0, 2), (2, 3), (2, 4), (2, 5), (4, 6)]
+        max_bonds = [3, 1, 4, 1, 3, 4, 1]
+
+        bb = fgs.FunctionalGroup(name='backbone',
+                                 charge=0,
+                                 elements=elements,
+                                 bonds=bonds,
+                                 max_bonds=max_bonds)
+
+        matches = bb.search(self.struct)
+        self.assertEqual(len(matches), 4)  # Same as no. of non-N-terminal residues
+
+        # Any Backbone (NH-CH-C-O)
+        #
+        #       H    H    O
+        #       |    |    |
+        # 2X -- N -- C -- C --
+        #            |
+        #            X
+        #
+        elements = [7, 1, 6, 1, 6, 0, 8]
+        bonds = [(0, 1), (0, 2), (2, 3), (2, 4), (2, 5), (4, 6)]
+
+        bb = fgs.FunctionalGroup(name='any-backbone',
+                                 charge=0,
+                                 elements=elements,
+                                 bonds=bonds)
+
+        matches = bb.search(self.struct)
+        self.assertEqual(len(matches), 6)  # Same as no. of residues
+
+        # Terminal amine
+        elements = [7, 1, 1, 1, 6]
+        bonds = [(0, 1), (0, 2), (0, 3), (0, 4)]
+        max_bonds = [4, 1, 1, 1, 4]
+        nt = fgs.FunctionalGroup(name='terminal-amine',
+                                 charge=1,
+                                 elements=elements,
+                                 bonds=bonds,
+                                 max_bonds=max_bonds)
+
+        matches = nt.search(self.struct)
+        self.assertEqual(len(matches), 2)
+
+
+class TestChargedFunctionalGroups(unittest.TestCase):
+    """Test all charged functional groups.
+    """
+
+    def setUp(self):
+        fpath = os.path.join(TESTDIR, 'data', 'mini.pdb')
+        self.struct = ia.read(fpath)
+
+    def test_findAnions(self):
+        """Tests matching anionic functional groups
+        """
+
+        anions = fgs.anionic
+        num_matches = 0
+        for fg in anions:
+            _fg = fg()
+            m = _fg.search(self.struct)
+            num_matches += len(m)
+
+        self.assertEqual(num_matches, 2)  # Two aspartates
+
+    def test_findCations(self):
+        """Tests matching cationic functional groups
+        """
+
+        cations = fgs.cationic
+        num_matches = 0
+        for fg in cations:
+            _fg = fg()
+            m = _fg.search(self.struct)
+            num_matches += len(m)
+
+        self.assertEqual(num_matches, 4)  # Two N-terminal Arginines
