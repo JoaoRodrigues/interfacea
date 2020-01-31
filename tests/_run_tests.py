405294d8041666b9aecc155823a15b8eb970a49d
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2020-01-31 13:55:55 -0800

    Moved old tests to avoid pytest discovery

---
+++ tests/_run_tests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 João Pedro Rodrigues
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Unit tests for read() function
+"""
+
+import os
+import sys
+import unittest
+
+if __name__ == '__main__':
+
+    mpath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    sys.path.insert(0, mpath)  # module path so we load dist files
+
+    loader = unittest.defaultTestLoader
+
+    tpath = os.path.join(mpath, 'tests')
+    suite = loader.discover(tpath)
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
