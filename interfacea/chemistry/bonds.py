bc58aa2f95e68b106cbef8cbe3797d940dd39791
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2020-03-20 18:13:27 -0700

    Refactored and completed bond analyzer.

--- interfacea/chemistry/bonds.py
+++ interfacea/chemistry/bonds.py
@@ -19,86 +19,121 @@
 Module containing bonding information and analyzers.
 """
 
+import abc
 import logging
 
-from interfacea.core.structure import Structure
+import networkx as nx
+
 from interfacea.chemistry.data import COVALENT_RADII
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
-class SimpleBondAnalyzer(object):
-    """Class to infer atom connectivity from cartesian coordinates.
+class BaseBondAnalyzer(abc.ABC):
+    """Base class for bond analyzers
 
-    Args:
-        structure (Structure): atomic structure with coordinate and metadata.
-
-    Attributes:
-        tolerance (float): factor to account for variations in bond lengths.
-            Set to 0.45 by default, lower means a more strict criterion to
-            consider two atoms bonded.
-        ignore (list): atomic numbers to ignore when accounting for bonding.
-            By default, contains all metallic elements.
-
-    Returns:
-        List of 2-item tuples with Atoms involved in a bond.
+    Subclasses must implement a run() method that takes a Structure object
+    as an argument and returns a networkx Graph defining bonds between all atoms
+    in the structure.
     """
 
-    def __init__(self, structure):
+    @abc.abstractmethod
+    def run(structure):
+        """Analyzes a structure to infer atom connectivity.
 
-        self.tolerance = 0.45
-        self.ignore = [
-            3, 4, 12, 13, 18, 23, 24, 25, 27, 29, 30, 31, 33, 36, 37, 38, 39,
-            42, 47, 48, 49, 51, 52, 54, 55, 56, 57, 58, 62, 63, 64, 65, 67,
-            70, 71, 74, 75, 76, 77, 78, 79, 80, 81, 82, 92,
-        ]
+        Arguments
+        ---------
+            structure : Structure
+                atomic structure with coordinate and metadata.
 
-        self._load_structure(structure)  # sets self.structure
+        Returns
+        -------
+            a networkx Graph object representing the bond graph.
+        """
+        pass
 
-    def _load_structure(self, structure):
-        """Validates the input structure object
 
-        Raises:
-            TypeError: if input structure is not a Structure type.
+class SimpleBondAnalyzer(BaseBondAnalyzer):
+    """Simplistic analyzer to infer atom connectivity from xyz coordinates"""
+
+    def __init__(self, tolerance=0.45, ignorelist=None):
+        """
+        Arguments
+        ---------
+            tolerance : float, optional
+                fudge factor (in Angstrom) to account for variations in bond
+                lengths. A lower value means a more strict criterion to consider
+                two atoms bonded, so less bonds inferred. Default is 0.45.
+            ignorelist : list, optional
+                atomic numbers to ignore when accounting for bonding. By default
+                contains all metallic elements.
+
+        Raises
+        ------
+            ValueError
+                if the tolerance value is zero or a negative number.
         """
 
-        if not isinstance(structure, Structure):
-            emsg = f"Object is not a Structure (sub)class: {type(structure)}"
-            raise TypeError(emsg)
+        if tolerance <= 0:
+            raise ValueError(f'Tolerance factor must be > 0')
 
-        self.structure = structure
+        if ignorelist is None:
+            ignorelist = [
+                3, 4, 12, 13, 18, 23, 24, 25, 27, 29, 30, 31, 33, 36, 37, 38, 39,
+                42, 47, 48, 49, 51, 52, 54, 55, 56, 57, 58, 62, 63, 64, 65, 67,
+                70, 71, 74, 75, 76, 77, 78, 79, 80, 81, 82, 92,
+            ]
 
-    def run(self):
-        """Runs the analyzer."""
+        self.ignoreset = set(ignorelist)
+        self.tolerance = tolerance
 
-        seen = set()  # store bonds we've seen not to repeat.
+    def run(self, structure):
+        """
+        Arguments
+        ---------
+            structure : Structure
+                atomic structure with coordinate and metadata.
 
-        ignore_set = set(self.ignore)
+        Returns
+        -------
+            a networkx Graph object representing the bond graph.
+        """
 
-        # Load kdtree
-        if self.structure._kdtree:
-            self.structure.generate_kdtree()
+        covradii = COVALENT_RADII
 
-        max_r = (max(COVALENT_RADII.values()) ** 2) + self.tolerance
-        get_r = lambda a: COVALENT_RADII.get(a.element.atomic_number, 2.0)
+        # Initialize bond graph with atoms as nodes
+        bondgraph = nx.Graph()
+        for atom in structure:
+            bondgraph.add_node(atom.serial, atom=atom)
 
-        for atom in self.structure:
-            if atom.element.atomic_number in ignore_set:
+        max_r = (max(covradii.values()) ** 2) + self.tolerance
+
+        seen = set()  # store atoms we've visited not to repeat.
+        for atom in structure:
+            if atom.element.atomic_number in self.ignoreset:
                 logging.debug(f"Skipping bonds for {atom}: ignored element")
                 continue
 
-            atom_r = get_r(atom)
+            atom_r = covradii.get(
+                atom.element.atomic_number,
+                2.0
+            )
 
-            nlist = atom.neighbors(radius=max_r)
+            nlist = structure.neighbors(atom, radius=max_r)
             for neighbor, d in nlist:
                 if (
                     neighbor in seen or  # noqa: W504
-                    neighbor.element.atomic_number in ignore_set
+                    neighbor.element.atomic_number in self.ignoreset
                 ):
                     continue
 
-                neighbor_r = get_r(neighbor)
+                neighbor_r = covradii.get(
+                    neighbor.element.atomic_number,
+                    2.0
+                )
                 if d <= (atom_r + neighbor_r + self.tolerance):
-                    yield (atom, neighbor)
+                    bondgraph.add_edge(atom.serial, neighbor.serial)
 
             seen.add(atom)
+
+        return bondgraph
